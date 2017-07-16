b8b647cbe0cc16174e4b68921172c52353a36883
DavLiet <davlietjauw@yahoo.com>
2017-07-16 17:14:02 -0500

    Added setMonitoring Listener to MyApplication

--- app/src/main/java/com/DavidLieTjauwWustlEdu/SandboxNasaKua/MyApplication.java
+++ app/src/main/java/com/DavidLieTjauwWustlEdu/SandboxNasaKua/MyApplication.java
@@ -3,6 +3,12 @@ package com.DavidLieTjauwWustlEdu.SandboxNasaKua;
 import android.app.Application;
 
 import com.estimote.coresdk.common.config.EstimoteSDK;
+import com.estimote.sdk.Beacon;
+import com.estimote.sdk.BeaconManager;
+import com.estimote.sdk.Region;
+
+import java.util.List;
+import java.util.UUID;
 
 //
 // Running into any issues? Drop us an email to: contact@estimote.com
@@ -10,11 +16,35 @@ import com.estimote.coresdk.common.config.EstimoteSDK;
 
 public class MyApplication extends Application {
 
+    private BeaconManager beaconManager;
 
 
     @Override
     public void onCreate() {
         super.onCreate();
+
+        beaconManager = new BeaconManager(getApplicationContext());
+        beaconManager.setMonitoringListener(new BeaconManager.MonitoringListener(){
+            @Override
+            public void onEnteredRegion(Region region, List<Beacon> list) {
+
+            }
+
+            @Override
+            public void onExitedRegion(Region region) {
+            }
+
+
+        });
+
+        beaconManager.connect(new BeaconManager.ServiceReadyCallback() {
+            @Override
+            public void onServiceReady() {
+                beaconManager.startMonitoring(new Region("monitored region",
+                        UUID.fromString("B9407F30-F5F8-466E-AFF9-25556B57FE6D"), 43556, 23583));
+            }
+        });
+
 
         EstimoteSDK.initialize(getApplicationContext(), "sandbox-nasa-kua", "17f63377066b3b5a18f12e6930815972");
 
