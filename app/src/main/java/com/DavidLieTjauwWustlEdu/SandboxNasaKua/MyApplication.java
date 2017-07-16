58008da11f636390ab8a5e9f3c964bdd122de498
DavLiet <davlietjauw@yahoo.com>
2017-07-16 17:16:24 -0500

    Added notifications

--- app/src/main/java/com/DavidLieTjauwWustlEdu/SandboxNasaKua/MyApplication.java
+++ app/src/main/java/com/DavidLieTjauwWustlEdu/SandboxNasaKua/MyApplication.java
@@ -1,6 +1,11 @@
 package com.DavidLieTjauwWustlEdu.SandboxNasaKua;
 
 import android.app.Application;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 
 import com.estimote.coresdk.common.config.EstimoteSDK;
 import com.estimote.sdk.Beacon;
@@ -27,11 +32,12 @@ public class MyApplication extends Application {
         beaconManager.setMonitoringListener(new BeaconManager.MonitoringListener(){
             @Override
             public void onEnteredRegion(Region region, List<Beacon> list) {
-
+                    showNotification("HERE", "I am");
             }
 
             @Override
             public void onExitedRegion(Region region) {
+                showNotification("Gone", "You are");
             }
 
 
@@ -51,5 +57,23 @@ public class MyApplication extends Application {
         // uncomment to enable debug-level logging
         // it's usually only a good idea when troubleshooting issues with the Estimote SDK
 //        EstimoteSDK.enableDebugLogging(true);
+    }
+
+    public void showNotification(String title, String message) {
+        Intent notifyIntent = new Intent(this, MainActivity.class);
+        notifyIntent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivities(this, 0,
+                new Intent[]{notifyIntent}, PendingIntent.FLAG_UPDATE_CURRENT);
+        Notification notification = new Notification.Builder(this)
+                .setSmallIcon(android.R.drawable.ic_dialog_info)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setAutoCancel(true)
+                .setContentIntent(pendingIntent)
+                .build();
+        notification.defaults |= Notification.DEFAULT_SOUND;
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(1, notification);
     }
 }
