5db7643708f9721686d7e24ee84f24b5131782c3
DavLiet <davlietjauw@yahoo.com>
2017-07-16 14:15:42 -0500

    Initial commit

---
+++ app/src/main/java/com/DavidLieTjauwWustlEdu/SandboxNasaKua/MyApplication.java
@@ -0,0 +1,23 @@
+package com.DavidLieTjauwWustlEdu.SandboxNasaKua;
+
+import android.app.Application;
+
+import com.estimote.coresdk.common.config.EstimoteSDK;
+
+//
+// Running into any issues? Drop us an email to: contact@estimote.com
+//
+
+public class MyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        EstimoteSDK.initialize(getApplicationContext(), "sandbox-nasa-kua", "17f63377066b3b5a18f12e6930815972");
+
+        // uncomment to enable debug-level logging
+        // it's usually only a good idea when troubleshooting issues with the Estimote SDK
+//        EstimoteSDK.enableDebugLogging(true);
+    }
+}
